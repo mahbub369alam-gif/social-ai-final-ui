ab0cd335c187922f74df21070382ede1a7966d99
mahbub369alam-gif <mahbub369alam@gmail.com>
2026-02-08 15:00:06 +0600

    Delete app directory

--- app/seller/inbox/page.tsx
+++
@@ -1,9 +0,0 @@
-import ChatDashboard from "@/components/ChatDashboard";
-
-export default function InboxPage() {
-  return (
-    <div className="h-[calc(100vh-56px)] md:h-screen">
-      <ChatDashboard />
-    </div>
-  );
-}
